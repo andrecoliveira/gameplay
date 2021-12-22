d8d9c74974d15f1256756e33baea6140e65d17ee
André Oliveira <andre.oliveira@delfosim.com>
2021-12-22 12:29:55 -0300

    Liftoff

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StatusBar } from 'react-native';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import IllustrationImg from '../../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn() {
     return (
         <View style={styles.container}>
-            <Text>Hello world!</Text>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="trasparent"
+                translucent
+            />
+
+            <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+            </View>
         </View>
     );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,31 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: theme.color.background,
         alignItems: 'center',
         justifyContent: 'center',
     },
+    image: {
+        width: '100%',
+        height: 360
+    },
+    content: {
+        marginTop: -40,
+        paddingHorizontal: 50
+    },
+    title: {
+        color: theme.color.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 16
+    },
+    subtitle: {
+        color: theme.color.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64
+    }
 });
